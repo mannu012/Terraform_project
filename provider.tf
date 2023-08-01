37b96b4c2762ce02b8a0bb9d379eea82e8eda7dc
mannu012 <49595424+mannu012@users.noreply.github.com>
2023-08-01 15:48:27 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -8,9 +8,9 @@ terraform {
 }
 
 provider "azurerm" {
-  subscription_id = "6912d7a0-bc28-459a-9407-33bbba641c07"
-  tenant_id = "70c0f6d9-7f3b-4425-a6b6-09b47643ec58"
-  client_id = "fb3eb97c-8679-488e-a4b4-5fd90299b7a2"
-  client_secret = "38o8Q~MsAPo59FK.Zsq0eQImB-vW2wrJznAUqc9V"
+  subscription_id = "Enter value"
+  tenant_id = "Enter value"
+  client_id = "Enter value"
+  client_secret = "Enter value"
   features {}  
 }
