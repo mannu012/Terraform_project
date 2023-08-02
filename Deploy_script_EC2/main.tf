0e7cd531496a4a8ec6bce937cc78e2412f28ac43
mannu012 <49595424+mannu012@users.noreply.github.com>
2023-08-02 20:16:53 +0530

    Update main.tf

--- Deploy_script_EC2/main.tf
+++ Deploy_script_EC2/main.tf
@@ -1,6 +1,6 @@
 provider "aws" {
-    access_key = "AKIASJ2YKAR4DL3NNPHW"
-    secret_key = "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"
+    access_key = "XXXXXXXXX"
+    secret_key = "XXXXXXX"
     region = "us-east-1"
 }
 
